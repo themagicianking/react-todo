12e8188999df1b79e2b7739f2493fa9ea93128df
themagicianking <tmoonder@gmail.com>
2024-02-26 11:43:32 -0500

    Added todo list items.

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let todoList = [
+  { id: 525600,
+    title: "finish homework"
+  },
+  { id: 420,
+    title: "feed the cats"},
+  { id: 96000,
+    title: "add work schedule to calendar"},
+  { id: 36,
+    title: "clean the apartment"},
+  { id: 9,
+    title: "go swimming"}
+];
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo List</h1>
+      <ul>{todoList.map(function (item) {
+        return <li key={item.id}>{item.title}</li>;
+      })}
+      </ul>
     </>
-  )
-}
+  );
+};
 
 export default App
