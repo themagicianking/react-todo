a9fac675e260fc6795bab44b72454c8b2f041687
themagicianking <tmoonder@gmail.com>
2024-03-01 15:33:28 -0500

    Moved list component to its own file and imported it into the main file.

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-let todoList = [
-  { id: 525600,
-    title: "finish homework"
-  },
-  { id: 420,
-    title: "feed the cats"},
-  { id: 96000,
-    title: "add work schedule to calendar"},
-  { id: 36,
-    title: "clean the apartment"},
-  { id: 9,
-    title: "go swimming"}
-];
+import './TodoList.jsx'
+import TodoList from './TodoList.jsx'
 
 function App() {
 
   return (
     <>
-      <h1>Todo List</h1>
-      <ul>{todoList.map(function (item) {
-        return <li key={item.id}>{item.title}</li>;
-      })}
-      </ul>
+    <h1>Todo List</h1>
+    <TodoList />
     </>
   );
 };
