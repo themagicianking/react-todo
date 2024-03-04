d5b4175749f9b04c0ada43c8571fd074b10f7c6c
themagicianking <tmoonder@gmail.com>
2024-03-03 19:06:13 -0500

    Added to do form--pushing so that I can complete in a different local environment.

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,12 @@
+const AddTodoForm = () => {
+  return (
+    <>
+      <form><label for="todoTitle">Title</label>
+        <input id="todoTitle"></input>
+        <input type="submit">Add</input>
+      </form>
+    </>
+  )
+};
+
+export default AddTodoForm
--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './TodoList.jsx'
 import TodoList from './TodoList.jsx'
+import AddTodoForm from './AddTodoForm.jsx'
 
 function App() {
 
@@ -11,6 +11,7 @@ function App() {
     <>
     <h1>Todo List</h1>
     <TodoList />
+    <AddTodoForm />
     </>
   );
 };
